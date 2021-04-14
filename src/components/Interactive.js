19f8fa95f340152248fe0ea9fc81bcdf87274c4a
Sneha Belkhale <sbelkhale@yahoo.com>
2021-04-14 16:53:08 -0400

    fixing on hover event

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -6,29 +6,37 @@ export default {
   },
 
   init: function () {
+    this.lerpT = 0;
 
     this.el.addEventListener("object3dset", () => {
       const mesh = this.el.getObject3D("mesh");
-      const scaleFactor = 0.1;
-
+      this.largeScale = new THREE.Vector3().copy(mesh.scale).multiplyScalar(1.1)
+      this.originalScale = new THREE.Vector3().copy(mesh.scale)
+      this.mesh = mesh;
+      this.mouseInside = false;
       this.el.addEventListener('click', (evt) => {
         window.open("https://github.com", '_blank');
       });
 
       this.el.addEventListener('mouseenter', (evt) => {
-        // evt.detail.intersection.point
-        mesh.scale.multiplyScalar(1 + scaleFactor);
+        this.mouseInside = true
       });
-  
       this.el.addEventListener('mouseleave', (evt) => {
-        mesh.scale.multiplyScalar(1 - scaleFactor);
+        this.mouseInside = false
       });
     })
 
     
   },
   
-  tick: function (time) {
-    
+  tick: function (time, dt) {
+    if(this.mouseInside) {
+      this.lerpT = Math.min(this.lerpT + dt/50, 1.0)
+    } else {
+      this.lerpT = Math.max(this.lerpT - dt/50, 0.0);
+    }
+    if(this.mesh) {
+      this.mesh.scale.copy(this.originalScale).lerp(this.largeScale, this.lerpT);
+    }
   },
 };
