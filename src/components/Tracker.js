d59336b0300efa4a090742316ce38c86ce4cc17f
kif11 <kovalewskiy@gmail.com>
2021-04-15 15:40:38 -0700

    Added align to Tracker

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -5,29 +5,35 @@ export default {
   schema: {
     el: { type: "string" },
     offset: { type: "vec3" },
+    alignH: { type: "string" },
+    alignW: { type: "string" },
   },
 
   init: function () {
+    // Set initial values
+    this.px = 0;
+    this.py = 0;
+    this.pz = 0;
+
     this.target = document.getElementById(this.data.el);
+
     const offset = this.data.offset;
+
     this.pos = this.el.getAttribute("position").clone().add(offset);
 
-    let cam = document.getElementById("camera");
-    this.camera = cam.object3D.children[0];
+    const cam = document.getElementById("camera");
+    this.camera = cam.object3D.getObjectByProperty('type', 'PerspectiveCamera');
 
     this.target.style.cssText = `
       position: absolute;
       top: 0px;
       left: 0px;
       transform-origin: 0px 0px 0px;
-      display: block;
       pointer-events: all;
     `;
-
-    console.log("[D] el:", this.target, this.screenPos);
   },
 
-  updateScreenPos: function() {
+  updateScreenPos: function () {
     this.camera.getWorldPosition()
     this.screenPos = new THREE.Vector3().copy(this.pos);
     this.screenPos.project(this.camera)
@@ -36,13 +42,17 @@ export default {
     this.py = (this.screenPos.y * -.5 + .5) * window.innerHeight;
     this.pz = 0;
 
-    //TODO: auto scale
-    this.scale = 1;
+    if (this.data.alignW === 'center') {
+      this.px -= this.target.offsetWidth/2
+    }
 
+    if (this.data.alignH === 'center') {
+      this.py -= this.target.offsetHeight/2
+    }
   },
 
   tick: function (time) {
     this.updateScreenPos();
-    this.target.style.transform = `translate3d(${this.px}px, ${this.py}px, ${this.pz}px) scale(${this.scale})`;
+    this.target.style.transform = `translate3d(${this.px}px, ${this.py}px, ${this.pz}px)`;
   },
 };
