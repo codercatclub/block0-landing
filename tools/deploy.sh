c10875f783971082c1809f104e6b455dc421a308
kif11 <kovalewskiy@gmail.com>
2021-05-14 13:16:35 -0700

    Adjusted contact responsivnes. Added deploy CNAME

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,9 +1,12 @@
 export NODE_ENV=production
 export HOST_URL="https://codercatclub.github.io/block0-landing/"
 
+domain="block-zero.us"
+
 yarn build
 
 touch dist/.nojekyll
+echo $domain > dist/CNAME
 
 git add -f dist/
 git commit -m "New github pages deploy"
